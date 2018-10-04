86b578c6204936f3cd1e9a88acae43f3c9dc6bbd
BlackSugarr <limeng0525@gmail.com>
2018-10-04 15:14:48 +0800

    add IO & NIO & Netty

---
+++ src/main/java/com/dimon/tryjava/myThread/threadlocal/ThreadLocalThread.java
@@ -0,0 +1,42 @@
+package com.dimon.tryjava.myThread.threadlocal;
+
+import java.util.concurrent.atomic.AtomicInteger;
+
+/**
+ * Created by limeng on 2018/10/3.
+ */
+public class ThreadLocalThread extends Thread
+{
+    private static AtomicInteger ai = new AtomicInteger();
+
+    public ThreadLocalThread(String name)
+    {
+        super(name);
+    }
+
+    public void run()
+    {
+        try
+        {
+            for (int i = 0; i < 3; i++)
+            {
+                Tools.t1.set(ai.addAndGet(1) + "");
+                System.out.println(this.getName() + " get value--->" + Tools.t1.get());
+                Thread.sleep(200);
+            }
+        }
+        catch (InterruptedException e)
+        {
+            e.printStackTrace();
+        }
+    }
+    public static void main(String[] args) throws Exception
+    {
+        ThreadLocalThread a = new ThreadLocalThread("ThreadA");
+        ThreadLocalThread b = new ThreadLocalThread("ThreadB");
+        ThreadLocalThread c = new ThreadLocalThread("ThreadC");
+        a.start();
+        b.start();
+        c.start();
+    }
+}
---
+++ src/main/java/com/dimon/tryjava/myThread/threadlocal/Tools.java
@@ -0,0 +1,9 @@
+package com.dimon.tryjava.myThread.threadlocal;
+
+/**
+ * Created by limeng on 2018/10/3.
+ */
+public class Tools
+{
+    public static ThreadLocal<String> t1 = new ThreadLocal<String>();
+}
