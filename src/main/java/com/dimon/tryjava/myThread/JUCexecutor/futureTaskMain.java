a93d61a41dd758894f2f5826faed3ec09b95957c
BlackSugarr <limeng0525@gmail.com>
2018-10-06 23:29:34 +0800

    add juc

---
+++ src/main/java/com/dimon/tryjava/myThread/JUCexecutor/futureTaskMain.java
@@ -0,0 +1,39 @@
+package com.dimon.tryjava.myThread.JUCexecutor;
+
+import java.util.concurrent.Callable;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.FutureTask;
+
+/**
+ * Created by limeng on 2018/10/5.
+ */
+public class futureTaskMain {
+
+    public static void main(String[] args) throws InterruptedException, ExecutionException {
+        FutureTask<String> task =
+                new FutureTask<>(new Task("test success"));
+        new Thread(task).start();
+        System.out.println("task result: " + task.get());
+    }
+}
+
+class Task implements Callable<String> {
+    private String result;
+
+    public Task(String result) {
+        this.result = result;
+    }
+
+    @Override
+    public String call() {
+        System.out.println("task is running ...");
+        try {
+            Thread.sleep(2000);
+        } catch (InterruptedException e) {
+            System.out.println("task is interrupted!");
+            return "(null)";
+        }
+        System.out.println("task is completed.");
+        return result;
+    }
+}
