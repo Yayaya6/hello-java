f8d3ca22e5748d137f8ac34489f2151524cac485
Dimon Li <dimon.li@ericsson.com>
2018-10-11 22:01:17 +0800

    add sth

---
+++ src/main/java/com/dimon/tryjava/myCollection/list/ListIteratorTest.java
@@ -0,0 +1,36 @@
+package com.dimon.tryjava.myCollection.list;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+
+public class ListIteratorTest
+{
+    private final static int LIST_SIZE = 1000;
+
+    public static void main(String[] args)
+    {
+        List<Integer> arrayList = new ArrayList<Integer>();
+        List<Integer> linkedList = new LinkedList<Integer>();
+
+        for (int i = 0; i < LIST_SIZE; i++)
+        {
+            arrayList.add(i);
+            linkedList.add(i);
+        }
+
+        long startTime = System.currentTimeMillis();
+        for (int i = 0; i < arrayList.size(); i++)
+        {
+            arrayList.get(i);
+        }
+        System.out.println("ArrayList遍历速度：" + (System.currentTimeMillis() - startTime) + "ms");
+
+        startTime = System.currentTimeMillis();
+        for (int i = 0; i < linkedList.size(); i++)
+        {
+            linkedList.get(i);
+        }
+        System.out.println("LinkedList遍历速度：" + (System.currentTimeMillis() - startTime) + "ms");
+    }
+}
